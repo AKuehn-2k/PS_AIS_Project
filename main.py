654657b74e0868610be1e1b989f4fbc86e0c8316
Waldemar <wgranson@stud.hs-heilbronn.de>
2023-07-05 00:39:14 +0200

    Extended Funtionality: After Translating the Script takes the Entries and creates new Nodes based on the "text:" and saves them  in their respective Collection using the "labels:" of the dictionary

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from libretranslatepy import LibreTranslateAPI
 import csv
-import os
 import json
+import os
+from arango import ArangoClient
+from libretranslatepy import LibreTranslateAPI
 
 # read csv file
 directory = './CSV_Input/'
 csv_file = max([os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.csv')], key=os.path.getctime)
+
+# Connect to ArangoDB
+client = ArangoClient(hosts="http://127.0.0.1:8529")
+db = client.db('academicontology')
 
 # Parse the CSV data and put into data list
 
@@ -48,9 +53,23 @@ for entry in data:
         translated_data.append(entry)
         print(entry)
 
+# Create nodes in ArangoDB
+for entry in translated_data:
+    collection_name = entry['labels'][0]  # Assumes only one label per entry
+    text = entry['text']
+
+    # Check if the collection exists, create it if not
+    if not db.has_collection(collection_name):
+        db.create_collection(collection_name)
+
+    # Insert the node document into the collection
+    collection = db.collection(collection_name)
+    collection.insert({'name': text})
+
+    print(f"Inserted node: {collection_name} - {text}")
+
 
 # write translated data into new json file output.json
-with open('output.json', 'w') as outfile:
-    json.dump(translated_data, outfile)
-    
-print("done")
+# with open('output.json', 'w') as outfile:
+    #json.dump(translated_data, outfile)
+
